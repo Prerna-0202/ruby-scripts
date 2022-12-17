7741f036be585cf25fe1374f1e1c54758a125798
Prerna Sharma <89515816+Prerna-0202@users.noreply.github.com>
2022-12-17 15:40:45 +0530

    Update BubbleSort.rb

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,20 +1,3 @@
-def bubble_sort(array)
-    unsorted = true
-    while unsorted do
-        i = 0
-        unsorted = false
-        while i < (array.length - 1)
-            if array[i] > array[i + 1]
-                t=array[i]
-                array[i]=array[i+1]
-                array[i+1]=t
-                unsorted = true
-            end
-            i += 1
-        end
-    end
-    print array   
-end
 def bubble_sort (array)
     unsorted=true
     while unsorted do
